f9c5a89602cc579b69235f455bc0f5886b6b69c1
evilicebergs <voiced.fair-0a@icloud.com>
2025-05-19 13:17:15 +0300

    Initial Commit

---
+++ echoCodeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echoCodeApp
+//
+//  Created by Artem Golovchenko on 2025-05-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ echoCodeApp/echoCodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoCodeAppApp.swift
+//  echoCodeApp
+//
+//  Created by Artem Golovchenko on 2025-05-19.
+//
+
+import SwiftUI
+
+@main
+struct echoCodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
